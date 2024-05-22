5d8b9eeadbf1918bab79a7d2c01c48278b6531c4
jjyj5264 <jjyj5264@gmail.com>
2024-05-23 00:31:14 +0900

    2024.05.23

---
+++ w12/upheap.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ELEMENT 200
+
+typedef struct {
+  int key;
+} element;
+
+typedef struct {
+  element heap[MAX_ELEMENT];
+  int heap_size;
+} HeapType;
+
+HeapType *create() {
+  return (HeapType *)malloc(sizeof(HeapType));
+}
+
+void init(HeapType *h) {
+  h->heap_size = 0;
+}
+
+void push_min_heap(HeapType *h, element item) {
+  int i = ++(h->heap_size);
+
+  while ((i != 1) && (item.key < h->heap[i / 2].key)) {
+    h->heap[i] = h->heap[i / 2];
+    i /= 2;
+  }
+
+  h->heap[i] = item;
+}
+
+element pop_min_heap(HeapType *h) {
+  int parent, child;
+  element item, temp;
+  item = h->heap[1];
+  temp = h->heap[(h->heap_size)--];
+  parent = 1;
+  child = 2;
+
+  while (child <= h->heap_size) {
+    // 현재 노드의 자식노드 중 더 큰 자식노드를 찾는다.
+    if ((child < h->heap_size) &&
+        (h->heap[child].key) > h->heap[child + 1].key)
+      child++;
+    if (temp.key <= h->heap[child].key)
+      break;
+    // 한 단계 아래로 이동
+    h->heap[parent] = h->heap[child];
+    parent = child;
+    child *= 2;
+  }
+
+  h->heap[parent] = temp;
+  return item;
+}
+
+int main() {
+  element e1 = {10}, e2 = {5}, e3 = {30};
+  element e4, e5, e6;
+  HeapType *heap;
+  heap = create(); // 힙 생성
+  init(heap);      // 초기화
+                   // 삽입
+  push_min_heap(heap, e1);
+  push_min_heap(heap, e2);
+  push_min_heap(heap, e3);
+  // 삭제
+  e4 = pop_min_heap(heap);
+  printf("< %d > ", e4.key);
+  e5 = pop_min_heap(heap);
+  printf("< %d > ", e5.key);
+  e6 = pop_min_heap(heap);
+  printf("< %d > \n", e6.key);
+  free(heap);
+  return 0;
+}
